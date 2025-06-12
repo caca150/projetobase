a529031f8843a22846d4d36d84fa05a432c7d762
caca150 <cardosoclara598@gmail.com>
2025-06-12 09:22:42 -0300

    antes de excluir aprendi funções

--- logicacomjs.js
+++ logicacomjs.js
@@ -65,44 +65,132 @@ else if (pontuacao>=5001 && pontuacao<=10000){
 } 
  else {
     alert("Parabéns!! Seu nível é master e seu bônus de XP é de: 2000XP")
- }*/
+ }
 let basico = 25;
 let premium = 40;
 let ultra = 60;
+let telas1a2 = 0;
 let telas3a4 = 5;
 let telasmaisde4 = 7;
 let tipodassinatura = prompt ("Digite o tipo de assinatura que deseja: ")
 let telas = prompt("Quantas telas deseja?")
-
-
-
-
-
-
 if (tipodassinatura == "básico "){
    if (telas>=1 && telas<=2 ){
-    alert("O valor do seu pacote séra de: " + basico+"R$")
+    alert("O valor do seu pacote séra de: R$" +(basico+telas1a2) )
    }
    else if (telas>=3 && telas<=4){
-     alert("O valor do seu pacote séra de: "+ (basico+telas3a4)+"R$")
+     alert("O valor do seu pacote séra de: R$"+ (basico+telas3a4))
 
    }
    else {
-
-   }
-   alert("O valor do seu pacote séra: "+(basico+telasmaisde4)+"R$")
+alert("O valor do seu pacote séra: R$"+(basico+telasmaisde4))
 }
+   }
+   
+
 else if (tipodassinatura == "premium"){
     if (telas>=1 && telas<=2 ){
-    alert("O valor do seu pacote séra de: R$" + premium +)
+    alert("O valor do seu pacote séra de: R$" +(premium+telas1a2)  )
    }
    else if (telas>=3 && telas<=4){
-     alert("O valor do seu pacote séra de: "+ (premium+telas3a4)+"R$")
+     alert("O valor do seu pacote séra de: R$"+ (premium+telas3a4))
 
    }
    else {
+alert("O valor do seu pacote séra de: R$ "+(premium+telasmaisde4))
+   }
+   
+}
+else{
+     if (telas>=1 && telas<=2 ){
+    alert("O valor do seu pacote séra de: R$ " + (ultra+telas1a2))
+   }
+   else if (telas>=3 && telas<=4){
+     alert("O valor do seu pacote séra de: R$"+ (ultra+telas3a4))
 
    }
-   alert("O valor do seu pacote séra: "+(premium+telasmaisde4)+"R$")
+   else {
+ alert("O valor do seu pacote séra: R$"+(ultra+telasmaisde4) )
+   }
+   }
+  let daysOfWeek = ["Caio","Samuel"];
+  console.log(daysOfWeek[1])
+  console.log(daysOfWeek.length)
+  let listdiver = ["Caio",1,2.5,"Escola",false]
+  console.log(listdiver[3])
+   let nomedoaluno = prompt("Digite o nome do novo aluno: ")
+   daysOfWeek.push(nomedoaluno) // anoação: push adiciona por ultimo 
+   console.log(daysOfWeek)
+
+   let daysOfWeek = ["Caio","Samuel"];
+  console.log(daysOfWeek[1])
+  console.log(daysOfWeek.length)
+  let listdiver = ["Caio",1,2.5,"Escola",false]
+  console.log(listdiver[3])
+   let nomedoaluno = prompt("Digite o nome do novo aluno: ")
+  daysOfWeek.unshift("caique", "lyann", "clara") // se colocar "" em cada vai sair so o ultimo (se colocar sem vai sair todos)
+   daysOfWeek.push(nomedoaluno) // unshift adiciona em primeiro lugar
+   daysOfWeek.unshift("Richard")
+ daysOfWeek.shift() // pop tira o ultimo e shift tira o primeiro
+   console.log(daysOfWeek)
+
+   let daysOfWeek = ["Caio","Samuel"];
+  console.log(daysOfWeek[1])
+  console.log(daysOfWeek.length)
+  let listdiver = ["Caio",1,2.5,"Escola",false]
+  console.log(listdiver[3])
+   let nomedoaluno = prompt("Digite o nome do novo aluno: ")
+  daysOfWeek.unshift("caique", "lyann", "clara") 
+   daysOfWeek.push(nomedoaluno) 
+   daysOfWeek.unshift("Richard")
+ daysOfWeek.sort() // bota em ordem alfabetica ou numerica
+ daysOfWeek.reverse()  // reverte os lugares
+ console.log("Os alunos são: "+daysOfWeek)
+   console.log(daysOfWeek.indexOf("Samuel"))
+
+   const alunos = ["Ana","Bruno","Carlos","Diana"];
+   for (const aluno  of alunos){
+    console.log("Olá, "+ aluno+"!");
+   }
+ let number = Number(prompt("Digite o número que deseja somar aos anteriores: "))
+ let numberfinal =0
+ while (number != 0){
+ number = Number(prompt("Digite o número que deseja somar aos anteriores: "))
+     numberfinal = numberfinal+number
+
 }
-else
+  console.log("O número final é: "+numberfinal)
+
+  var array = [1,2,,3,4,5,6,7,8,9,10,11]
+  for(let i = 1; i <=10; i++){ 
+ console.log(i);
+  }
+ 
+ function saudar (){
+alert("Seja bem-vindo ao sistema")
+
+ }
+saudar()
+
+function dobrarNumero(numero){
+  return numero * 2;
+}
+let resultado =dobrarNumero(20);
+ alert("O resultado é: " + resultado)
+
+ function comprimentar(nome){
+  alert("Oi, "+ nome +" tenha um excelente dia!")
+ }
+ 
+ comprimentar("Davy")
+
+ function contagemregressiva(numero){
+alert(numero)
+if(numero ===0){
+  alert("Chegou")
+}
+contagemregressiva(numero-1)
+ }
+ contagemregressiva(5);*/
+
+ 
