ed3ba52969fb65fb37a9a3a2133bada869fe1a71
caca150 <cardosoclara598@gmail.com>
2025-06-12 11:31:25 -0300

    document e event

---
+++ cadastro.js
@@ -0,0 +1,8 @@
+function clicou(){
+    let quandoclicar = document.getElementById("botao")
+    quandoclicar.innerText = "Clicou"
+}
+function saiu(){
+    let quandosair = document.getElementById("botao")
+    quandosair.style.background = "green"
+}
---
+++ index.js
@@ -0,0 +1,9 @@
+/*let buscando = document.getElementsByTagName("li")[2]
+console.log(buscando)
+let buscando = document.getElementsByTagName("h2dadiv")[0]
+console.log(buscando)
+let buscando = document.getElementById("atalaia")
+buscando.style.background = "gray"
+GetElementsClassName
+let modorapido = document.querySelector("atalaia")
+modorapido.style.background = "red"*/
